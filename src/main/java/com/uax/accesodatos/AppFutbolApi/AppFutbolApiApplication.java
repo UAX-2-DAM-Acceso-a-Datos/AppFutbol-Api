23c1a04d537cbf86b33d6d4fe33c9513d5af3503
MI-NOTEBOOK-PRO\German <germfema@uax.es>
2023-02-09 11:03:43 +0100

    Commit inicial

---
+++ src/main/java/com/uax/accesodatos/AppFutbolApi/AppFutbolApiApplication.java
@@ -0,0 +1,13 @@
+package com.uax.accesodatos.AppFutbolApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppFutbolApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppFutbolApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uax/accesodatos/AppFutbolApi/AppFutbolApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uax.accesodatos.AppFutbolApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppFutbolApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
