682a9ba7f20d13592e7895bae0028b2d4d9bd125
Medra235 <102791570+Medra235@users.noreply.github.com>
2023-02-13 10:40:06 +0100

    Modificación del pom

--- src/main/java/com/uax/accesodatos/AppFutbolApi/Controller/WelcomeController.java
+++ src/main/java/com/uax/accesodatos/AppFutbolApi/Controller/WelcomeController.java
@@ -1,5 +1,13 @@
 package com.uax.accesodatos.AppFutbolApi.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class WelcomeController {
 
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
 }
