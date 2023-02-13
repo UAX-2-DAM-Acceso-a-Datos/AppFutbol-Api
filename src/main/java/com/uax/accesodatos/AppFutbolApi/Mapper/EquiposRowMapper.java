e4b00ea79509742e895a45c5f3704df71c5d3b9e
Medra235 <102791570+Medra235@users.noreply.github.com>
2023-02-13 10:27:50 +0100

    RowMapper de equipos

---
+++ src/main/java/com/uax/accesodatos/AppFutbolApi/Mapper/EquiposRowMapper.java
@@ -0,0 +1,26 @@
+package com.uax.accesodatos.AppFutbolApi.Mapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import com.uax.accesodatos.AppFutbolApi.DTO.EquiposDTO;
+
+public class EquiposRowMapper implements RowMapper<EquiposDTO>{
+
+	@Override
+	public EquiposDTO mapRow(ResultSet rs, int rowNum) throws SQLException {
+        EquiposDTO equipo = new EquiposDTO(
+                rs.getInt("id"),
+                rs.getString("name"),
+                rs.getString("season"),
+                rs.getString("country"),
+                rs.getString("logo"),
+                rs.getString("urlimage"),
+                rs.getString("venue")
+            );
+            return equipo;
+	}
+
+}
