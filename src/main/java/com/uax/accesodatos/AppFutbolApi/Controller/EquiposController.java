4210ec484e6ceee867acd9b7e7ca67c4a1fae407
Medra235 <102791570+Medra235@users.noreply.github.com>
2023-02-13 11:02:00 +0100

    Services de equipos

--- src/main/java/com/uax/accesodatos/AppFutbolApi/Controller/EquiposController.java
+++ src/main/java/com/uax/accesodatos/AppFutbolApi/Controller/EquiposController.java
@@ -1,5 +1,32 @@
 package com.uax.accesodatos.AppFutbolApi.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.uax.accesodatos.AppFutbolApi.DTO.EquiposDTO;
+import com.uax.accesodatos.AppFutbolApi.Services.EquiposService;
+
+@Controller
 public class EquiposController {
+	
+	@Autowired
+	private EquiposService equiposService;
+
+	@GetMapping("/equipos")
+	public String mostrarEquipos(@RequestParam(name = "busqueda", required = false) String busqueda, Model model) {
+	    List<EquiposDTO> equipos;
+	    if (busqueda != null) {
+	        equipos = equiposService.buscarEquipos(busqueda);
+	    } else {
+	        equipos = equiposService.findAll();
+	    }
+	    model.addAttribute("equipos", equipos);
+	    return "listaEquipos";
+	}
 
 }
---
+++ src/main/java/com/uax/accesodatos/AppFutbolApi/Services/EquiposService.java
@@ -0,0 +1,61 @@
+package com.uax.accesodatos.AppFutbolApi.Services;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.jdbc.core.JdbcTemplate;
+import com.uax.accesodatos.AppFutbolApi.DTO.EquiposDTO;
+import com.uax.accesodatos.AppFutbolApi.DTO.EquiposResponseDTO;
+
+import org.springframework.jdbc.core.RowMapper;
+
+@Service
+public class EquiposService {
+	
+	    
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    
+    @Autowired
+    private RestTemplate restTemplate;
+    public List<EquiposDTO> findAll() {
+        String sql = "SELECT * FROM equipos";
+        List<EquiposDTO> equipos = jdbcTemplate.query(sql, new RowMapper<EquiposDTO>() {
+            @Override
+            public EquiposDTO mapRow(ResultSet rs, int rowNum) throws SQLException {
+                EquiposDTO equipo = new EquiposDTO(
+                    rs.getInt("id"),
+                    rs.getString("name"),
+                    rs.getString("season"),
+                    rs.getString("country"),
+                    rs.getString("logo"),
+                    rs.getString("urlimage"),
+                    rs.getString("venue")
+                );
+                return equipo;
+            }
+        });
+        return equipos;
+    }
+    public List<EquiposDTO> buscarEquipos(String busqueda) {
+        String url = "https://v3.football.api-sports.io/teams?search=%s" + busqueda;
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("x-rapidapi-host", "v3.football.api-sports.io");
+        headers.set("x-rapidapi-key", "562a0587497cee9a309f88f5c8bcb789");
+
+        HttpEntity<String> requestEntity = new HttpEntity<>(null, headers);
+        ResponseEntity<EquiposResponseDTO> responseEntity = restTemplate.exchange(url, HttpMethod.GET, requestEntity, EquiposResponseDTO.class);
+
+        EquiposResponseDTO equiposResponse = responseEntity.getBody();
+        List<EquiposDTO> equipos = equiposResponse.getData();
+        return equipos;
+    }
+}
