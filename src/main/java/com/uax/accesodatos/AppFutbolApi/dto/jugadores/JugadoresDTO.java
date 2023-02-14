44140a8bde10a53d079719fed7282b65eb571012
Medra235 <102791570+Medra235@users.noreply.github.com>
2023-02-14 13:57:39 +0100

    no message

--- src/main/java/com/uax/accesodatos/AppFutbolApi/dto/jugadores/JugadoresDTO.java
+++ src/main/java/com/uax/accesodatos/AppFutbolApi/dto/jugadores/JugadoresDTO.java
@@ -2,6 +2,72 @@ package com.uax.accesodatos.AppFutbolApi.dto.jugadores;
 
 public class JugadoresDTO {
 	
+	public int id;
+	public String nombre;
+	public int edad;
+	public String nacionalidad;
+	public String urlfoto;
+	public int equipo;
+	
+	public JugadoresDTO(int id, String nombre, int edad, String nacionalidad, String urlfoto, int equipo) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.edad = edad;
+		this.nacionalidad = nacionalidad;
+		this.urlfoto = urlfoto;
+		this.equipo = equipo;
+	}
+
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getNacionalidad() {
+		return nacionalidad;
+	}
+
+	public void setNacionalidad(String nacionalidad) {
+		this.nacionalidad = nacionalidad;
+	}
+
+	public String getUrlfoto() {
+		return urlfoto;
+	}
+
+	public void setUrlfoto(String urlfoto) {
+		this.urlfoto = urlfoto;
+	}
+
+	public int getEquipo() {
+		return equipo;
+	}
+
+	public void setEquipo(int equipo) {
+		this.equipo = equipo;
+	}
+	
 	
 
 }
--- src/main/java/com/uax/accesodatos/AppFutbolApi/services/JugadoresService.java
+++ src/main/java/com/uax/accesodatos/AppFutbolApi/services/JugadoresService.java
@@ -1,5 +1,13 @@
 package com.uax.accesodatos.AppFutbolApi.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
 public class JugadoresService {
+	
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
 }
