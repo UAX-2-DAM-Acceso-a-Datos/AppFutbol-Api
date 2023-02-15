c2cf2aa7986adc77d941314b7a2d0fbdf900f76b
Medra235 <102791570+Medra235@users.noreply.github.com>
2023-02-15 08:34:59 +0100

    DTO

--- src/main/java/com/uax/accesodatos/AppFutbolApi/services/EquiposService.java
+++ src/main/java/com/uax/accesodatos/AppFutbolApi/services/EquiposService.java
@@ -40,6 +40,7 @@ public class EquiposService {
                     rs.getString("logo"),
                     rs.getString("urlimage"),
                     rs.getString("venue")
+                    
                 );
                 return equipo;
             }
--- src/main/java/com/uax/accesodatos/AppFutbolApi/services/JugadoresService.java
+++ src/main/java/com/uax/accesodatos/AppFutbolApi/services/JugadoresService.java
@@ -1,13 +1,38 @@
 package com.uax.accesodatos.AppFutbolApi.services;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.uax.accesodatos.AppFutbolApi.dto.jugadores.JugadoresDTO;
 
 @Service
 public class JugadoresService {
 	
     @Autowired
     private JdbcTemplate jdbcTemplate;
+    
+    @Autowired
+    private RestTemplate restTemplate;
+    public List<JugadoresDTO> findAll() {
+        String sql = "SELECT * FROM jugadores";
+        List<JugadoresDTO> jugadores = jdbcTemplate.query(sql, new RowMapper<JugadoresDTO>() {
+            @Override
+            public JugadoresDTO mapRow(ResultSet rs, int rowNum) throws SQLException {
+            	JugadoresDTO jugador = new JugadoresDTO(
+                    rs.getString("id"),
+                    rs.getString(""),
+                );
+                return jugador;
+            }
+        });
+        return jugadores;
+    }
 
 }
